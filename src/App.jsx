5c954995610f2d64c3f620a683a000607258ca42
maya <maya.e@webased.net>
2024-10-29 14:49:58 -0700

    persist data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {useState} from "react"
+import {useState , useEffect} from "react"
 import TodoInput  from "./components/TodoInput"
 import TodoList  from "./components/TodoList"
 
@@ -8,22 +8,37 @@ function App() {
   const [todoValue , setTodoValue] = useState('');//tp access value in input component by list for edit handler
   //make it share with parent component
 
+
+  const persistData = (newList)=>{
+    localStorage.setItem('todos' , JSON.stringify({todos:newList}))
+  }
   const handleAddTodos = (newTodo) => {
     const newTodosList = [...todos , newTodo]
+    persistData(newTodosList)
     setTodos(newTodosList);
     
   };
   const deleteTodoHandler =(todoIndx)=>{
       const newTodoList = todos.filter((todo , indx  )=> {return(indx !== todoIndx)} );
+      persistData(newTodoList)
       setTodos(newTodoList)
   }
   const editTodoHandler = (todoIndx )=>{
     const valueToBeEdited = todos[todoIndx];
-    console.log(valueToBeEdited)
     setTodoValue(valueToBeEdited)
     deleteTodoHandler(todoIndx)//delete the old one
   }
 
+  useEffect(()=>{
+    if(!localStorage)
+      return
+    let localTodos = localStorage.getItem('todos')
+    if(!localTodos)
+      return
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  },[])//empty dependency to run whenver load page
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
